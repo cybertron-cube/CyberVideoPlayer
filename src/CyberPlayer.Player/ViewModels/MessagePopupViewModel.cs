5efe92d1de30d2dfe8693b2c68ec04c364dbf27a
cybertron-cube <54194687+cybertron-cube@users.noreply.github.com>
2023-07-25 01:39:10 -0400

    Create message markdown popup view
    Avalonia.Svg.Skia is included in Markdown.Avalonia so remove duplicate reference

---
+++ src/CyberPlayer.Player/ViewModels/MessagePopupViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Avalonia.Controls;
+using DynamicData.Binding;
+using ReactiveUI;
+
+namespace CyberPlayer.Player.ViewModels;
+
+public class MessagePopupViewModel : ViewModelBase, IDialogContent
+{
+    public IObservable<bool> CloseDialog { get; }
+    
+    private bool _close;
+    
+    public bool Close
+    {
+        get => _close;
+        set => this.RaiseAndSetIfChanged(ref _close, value);
+    }
+
+    private string _message = string.Empty;
+
+    public string Message
+    {
+        get => _message;
+        set => this.RaiseAndSetIfChanged(ref _message, value);
+    }
+
+    private string _title = string.Empty;
+
+    public string Title
+    {
+        get => _title;
+        set => this.RaiseAndSetIfChanged(ref _title, value);
+    }
+
+    public MessagePopupViewModel()
+    {
+        CloseDialog = this.WhenValueChanged(x => x.Close);
+
+        if (Design.IsDesignMode)
+        {
+            _title = "Yeet SKEET!";
+            _message = """
+            # Release 1.0.5
+            ## Features
+            + Add support for OSX
+            + More log information
+            + Additional setting to detach ffmpeg process
+               + When `DetachFFmpegProcess` is set to `true` ffmpeg will run and show up in its own terminal like normal
+               + Note that this will mean progress does not update in the UI while ffmpeg is running
+            ## BugFixes
+            + Fixed choosing no on overwrite prompt
+            + Fixed ffmpeg speed degrading to basically a stand still on certain video codecs
+
+            ## What's Changed
+            * Release 1.0.5 by @Blitznir in https://github.com/Blitznir/FFmpegAvalonia/pull/2
+
+
+            **Full Changelog**: https://github.com/Blitznir/FFmpegAvalonia/compare/1.0.4...1.0.5
+            """;
+        }
+    }
+}
---
+++ src/CyberPlayer.Player/Views/MessagePopupView.axaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CyberPlayer.Player.Views;
+
+public partial class MessagePopupView : UserControl
+{
+    public MessagePopupView()
+    {
+        InitializeComponent();
+        ButtonPanel = this.FindControl<StackPanel>("ButtonPanel");
+        if (ButtonPanel == null) throw new NullReferenceException();
+        if (Design.IsDesignMode)
+        {
+            ButtonPanel.Children.Add(new Button
+            {
+                Content = "Yes"
+            });
+            ButtonPanel.Children.Add(new Button
+            {
+                Content = "No"
+            });
+        }
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}

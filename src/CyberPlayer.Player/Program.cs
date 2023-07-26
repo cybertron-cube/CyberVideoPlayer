89ba4eb20ce8e8b7dffeead7a4ef79ba721d9211
cybertron-cube <54194687+cybertron-cube@users.noreply.github.com>
2023-07-26 01:28:44 -0400

    Use TrimmerRootDescriptor

--- src/CyberPlayer.Player/Program.cs
+++ src/CyberPlayer.Player/Program.cs
@@ -1,9 +1,7 @@
 ﻿using Avalonia;
 using Avalonia.ReactiveUI;
 using System;
-using System.Diagnostics.CodeAnalysis;
 using CyberPlayer.Player.Helpers;
-using CyberPlayer.Player.ViewModels;
 
 namespace CyberPlayer.Player
 {
@@ -12,7 +10,6 @@ namespace CyberPlayer.Player
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
-        [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(MpvPlayer))]
         [STAThread]
         public static void Main(string[] args)
         {

008dad14adc0d2b1ea6a275956c568d3aa4d5780
cybertron-cube <54194687+cybertron-cube@users.noreply.github.com>
2024-03-28 05:08:34 -0400

    Update VideoOverlayWindow.cs
    Greatly simplify the code, confirmed working on windows 10

--- src/CyberPlayer.Player/Views/VideoOverlayWindow.cs
+++ src/CyberPlayer.Player/Views/VideoOverlayWindow.cs
@@ -6,72 +6,70 @@ using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Media;
 using ReactiveMarbles.ObservableEvents;
+using Serilog;
 
 namespace CyberPlayer.Player.Views;
 
 public class VideoOverlayWindow : Window
 {
     private readonly CompositeDisposable? _disposables;
+
+    private readonly MainWindow _parent;
     
     public VideoOverlayWindow(MainWindow parent)
     {
-        const int systemDecorationsTop = 28; //only for testing
+        _parent = parent;
         
         var panel = new Border
         {
             Background = Brushes.Green,
+            Opacity = 0.3
             //CornerRadius = new CornerRadius(0, 0, 10, 10)
         };
         Content = panel;
         SystemDecorations = SystemDecorations.None;
         TransparencyLevelHint = new[] { WindowTransparencyLevel.Transparent };
         Background = Brushes.Transparent;
-        Position = parent.Position.WithY(parent.Position.Y + systemDecorationsTop);
-        Height = parent.MainGrid.RowDefinitions[0].ActualHeight + parent.MainGrid.RowDefinitions[1].ActualHeight;
-        Width = parent.FrameSize!.Value.Width;
+        
+        Log.Debug("Parent: {ParentPosition}", _parent.Position);
+        UpdatePosition();
+        UpdateSize();
+        
         ShowInTaskbar = false;
         
         //maximize/minimize/fullscreen
         //native menu?
         
-        panel.ContextMenu = parent.VideoPanel.ContextMenu;
-        panel.ContextMenu.DataContext = parent.ViewModel;
+        panel.ContextMenu = _parent.VideoPanel.ContextMenu;
+        panel.ContextMenu.DataContext = _parent.ViewModel;
         
-        panel.PointerPressed += parent.VideoPanel_OnPointerPressed;
+        panel.PointerPressed += _parent.VideoPanel_OnPointerPressed;
         //panel.DoubleTapped += VideoPanel_OnDoubleTapped;
 
-        var parentEvents = parent.Events();
+        var parentEvents = _parent.Events();
         
-        var pointerEnteredVideoPanel = Observable.FromEventPattern<PointerEventArgs>(action => parent.VideoPanel.AddHandler(PointerEnteredEvent, action),
-            action => parent.VideoPanel.RemoveHandler(PointerEnteredEvent, action));
-        
+        var pointerEnteredVideoPanel = Observable.FromEventPattern<PointerEventArgs>(action => _parent.VideoPanel.AddHandler(PointerEnteredEvent, action),
+            action => _parent.VideoPanel.RemoveHandler(PointerEnteredEvent, action));
+
         _disposables = new CompositeDisposable
         {
-            parentEvents.PositionChanged.Subscribe(x =>
-            {
-                var newPosition = x.Point.WithY(x.Point.Y + systemDecorationsTop);
-                if (parent._menuBarActivated)
-                {
-                    newPosition += new PixelPoint(0, (int)parent.MainGrid.RowDefinitions[0].ActualHeight);
-                }
-
-                Position = newPosition;
-            }),
-            parentEvents.SizeChanged.Subscribe(x =>
-            {
-                Width = x.NewSize.Width;
-            
-                var newHeight = parent.MainGrid.Bounds.Height - parent.MainGrid.RowDefinitions[2].ActualHeight;
-                if (parent._menuBarActivated)
-                {
-                    newHeight -= parent.MainGrid.RowDefinitions[0].ActualHeight;
-                }
-            
-                Height = newHeight;
-            }),
-            //On macOS sometimes this window somehow is put below the parent when interacting with the parent
-            pointerEnteredVideoPanel.Subscribe(_ => { Activate(); parent.Activate(); }),
+            parentEvents.PositionChanged.Subscribe(_ => UpdatePosition()),
+            // Video panel should always resize when main window resizes because it is proportionally sized
+            parentEvents.SizeChanged.Subscribe(x => UpdateSize()),
+            // On macOS sometimes this window is somehow put below the parent when interacting with the parent
+            pointerEnteredVideoPanel.Subscribe(_ => { Activate(); _parent.Activate(); }),
             this.Events().Closing.Subscribe(_ => _disposables?.Dispose())
         };
+    }
+
+    private void UpdatePosition()
+    {
+        Position = _parent.VideoPanel.PointToScreen(new Point(0, 0));
+    }
+
+    private void UpdateSize()
+    {
+        Width = _parent.VideoPanel.Bounds.Width;
+        Height = _parent.VideoPanel.Bounds.Height;
     }
 }

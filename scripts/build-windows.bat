2a2ccd981a7e0ba0eb2b2ad00c629ffae2671100
Matthias <54194687+cybertron-cube@users.noreply.github.com>
2024-04-30 02:09:57 -0400

    build: Remove single/multi from windows script

--- scripts/build-windows.bat
+++ scripts/build-windows.bat
@@ -7,8 +7,6 @@ set /p Choice=Build updater? (y/n):
 
 set /p Version=Enter version number: 
 
-set /p Build=Single (s), multi (m), or both?: 
-
 set /p Installer=Create installer? (y/n): 
 
 if %Installer%==y (
@@ -18,25 +16,9 @@ if %Installer%==y (
 )
 
 if %Choice%==y (
-    if %Build%==s (
-        py build.py -del -version %Version% -compile win-x64-single -buildupdater -resetversion -cpymds -cpyffmpeg -cpympv -cpymediainfo -cpyupdater -rmpdbs -delbinrel -zip%Installer%
-    ) else (
-        if %Build%==m (
-            py build.py -del -version %Version% -compile win-x64-multi -buildupdater -resetversion -cpymds -cpyffmpeg -cpympv -cpymediainfo -cpyupdater -rmpdbs -delbinrel -zip
-        ) else (
-            py build.py -del -version %Version% -compile win-x64-single;win-x64-multi -buildupdater -resetversion -cpymds -cpyffmpeg -cpympv -cpymediainfo -cpyupdater -rmpdbs -delbinrel -zip%Installer%
-        )
-    )
+    py build.py -del -version %Version% -compile win-x64 -buildupdater -cpymds -cpyffmpeg -cpympv -cpymediainfo -cpyupdater -rmpdbs -delbinrel -zip%Installer% -resetversion
 ) else (
-    if %Build%==s (
-        py build.py -del -version %Version% -compile win-x64-single -resetversion -cpymds -cpyffmpeg -cpympv -cpymediainfo -cpyupdater -rmpdbs -delbinrel -zip%Installer%
-    ) else (
-        if %Build%==m (
-            py build.py -del -version %Version% -compile win-x64-multi -resetversion -cpymds -cpyffmpeg -cpympv -cpymediainfo -cpyupdater -rmpdbs -delbinrel -zip
-        ) else (
-            py build.py -del -version %Version% -compile win-x64-single;win-x64-multi -resetversion -cpymds -cpyffmpeg -cpympv -cpymediainfo -cpyupdater -rmpdbs -delbinrel -zip%Installer%
-        )
-    )
+    py build.py -del -version %Version% -compile win-x64 -cpymds -cpyffmpeg -cpympv -cpymediainfo -cpyupdater -rmpdbs -delbinrel -zip%Installer% -resetversion
 )
 
 pause

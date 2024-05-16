4dfd4abfcee2da4a7d4b9662434f2bdbd20acb0a
Matthias <54194687+cybertron-cube@users.noreply.github.com>
2024-05-16 19:32:02 -0400

    fix: Display enum settings as strings in json
    JsonConverter attribute does not work with AOT compilation

--- src/CyberPlayer.Player/AppSettings/Settings.cs
+++ src/CyberPlayer.Player/AppSettings/Settings.cs
@@ -13,7 +13,6 @@ public class Settings
 {
     public bool UpdaterIncludePreReleases { get; set; } = false;
 
-    [JsonConverter(typeof(JsonStringEnumConverter))]
     public LogEventLevel LogLevel { get; set; } = LogEventLevel.Information;
 
     public int LogInstances { get; set; } = 5;
@@ -22,7 +21,6 @@ public class Settings
 
     public int TimeCodeLength { get; set; } = 8;
 
-    [JsonConverter(typeof(JsonStringEnumConverter))]
     public Renderer Renderer { get; set; } = Renderer.Hardware;
 
     public bool AutoCenter { get; set; } = true;
